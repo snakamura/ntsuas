ed3b33f8825e85f0b20e641af2221aff076e83c9
Satoshi Nakamura <snakamura@asteria.com>
2024-09-15 15:58:23 +0900

    Use asStateFlow

--- app/src/main/java/org/snak/ntsuas/model/Vario.kt
+++ app/src/main/java/org/snak/ntsuas/model/Vario.kt
@@ -2,14 +2,13 @@ package org.snak.ntsuas.model
 
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class Vario {
-    val altitude: StateFlow<Double>
-        get() = this._altitude
+    private val _altitude: MutableStateFlow<Double> = MutableStateFlow(0.0)
+    val altitude: StateFlow<Double> = this._altitude.asStateFlow()
 
     suspend fun setAltitude(altitude: Double) {
         this._altitude.emit(altitude)
     }
-
-    private val _altitude: MutableStateFlow<Double> = MutableStateFlow(0.0)
 }
